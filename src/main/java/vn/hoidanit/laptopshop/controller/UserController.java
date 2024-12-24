325d447927c6434582b503997567c541372f8262
Tinas0603 <phamvutien03@gmail.com>
2024-12-24 16:19:47 +0700

    setup JSTL in view

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,10 +1,9 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,8 +16,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
 
