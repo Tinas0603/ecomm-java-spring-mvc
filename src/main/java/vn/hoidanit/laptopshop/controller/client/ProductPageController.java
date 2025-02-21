7725d46691b3511ade1bd33a5be987dc73832abb
Tinas0603 <phamvutien03@gmail.com>
2025-02-21 19:51:18 +0700

    Xong trang homepage và xem chi tiết 1 sản phẩm

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ProductPageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ProductPageController.java
@@ -1,14 +1,35 @@
 package vn.hoidanit.laptopshop.controller.client;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ProductPageController {
+    final ProductService productService;
+
+    public ProductPageController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getProductDetailPage(Model model, @PathVariable long id) {
+        List<Product> products = productService.getAllProducts();
+        // Lấy danh sách category duy nhất từ products
+        Set<String> categories = products.stream()
+                .map(Product::getTarget)
+                .collect(Collectors.toSet());
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("product", this.productService.getProductById(id));
         return "client/product/detail";
     }
 }
